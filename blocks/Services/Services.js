78825d8257476c75e8c90c44ed849fbc95e877a8
William Stam <williamstam@gmail.com>
2021-06-17 14:25:48 +0100

    [Services] Add service and map out content

--- blocks/Services/Services.js
+++ blocks/Services/Services.js
@@ -7,69 +7,31 @@ import logo from '../../public/flowers.jpg'
 import classes from './Services.module.css'
 
 const Services = React.forwardRef(function Services(props, ref) {
-  const { className, ...other } = props
+  const { className, services, ...other } = props
 
   return (
     <Section className={classnames(classes.root, className)} ref={ref} {...other}>
-      <div className={classes.card}>
-        <div className={classes.imgContainer}>
-        <Image
-        className={classes.cardImg}
-        src={logo}
-        alt="picture of flowers"
-        height={250}
-        width={250}
-        />
-          {/* <img className={classes.cardImg} src={Flower} alt="random" /> */}
-        </div>
+      {services.map((service, id) => (
+        <div className={classes.card} key={id}>
+          <div className={classes.imgContainer}>
+            {/* <Image
+            className={classes.cardImg}
+            src={service.img}
+            alt="picture of flowers"
+            height={250}
+            width={250}
+          /> */}
+          </div>
 
-        <div className={classes.titleContainer}>
-          <h2 className={classes.cardTitle}>Custom Logo Design</h2>
-        </div>
+          <div className={classes.titleContainer}>
+            <h2 className={classes.cardTitle}>{service.title}</h2>
+          </div>
 
-        <div className={classes.infoContainer}>
-          <p className={classes.cardInfo}>
-            Give your new business an eye catching logo or refreshen the brand
-            of your existing business with a new one. Give your social media a brand spanking new
-            profile image and banner to show it off to your followers.
-          </p>
+          <div className={classes.infoContainer}>
+            <p className={classes.cardInfo}>{service.info}</p>
+          </div>
         </div>
-      </div>
-
-      <div className={classes.card}>
-        <div className={classes.imgContainer}>
-          {/* <img className={classes.cardImg} src={Flower} alt="random" /> */}
-        </div>
-
-        <div className={classes.titleContainer}>
-          <h2 className={classes.cardTitle}>Print Based Media</h2>
-        </div>
-
-        <div className={classes.infoContainer}>
-          <p className={classes.cardInfo}>
-          There&apos;s still space in the world for print based media such as business cards,
-          flyers and brochures to communicate with your customers directly.
-          Or perhaps you need custom packaging for your product. If you can think of it, we can create it together!
-          </p>
-        </div>
-      </div>
-
-      <div className={classes.card}>
-        <div className={classes.imgContainer}>
-          {/* <img className={classes.cardImg} src={Flower} alt="random" /> */}
-        </div>
-
-        <div className={classes.titleContainer}>
-          <h2 className={classes.cardTitle}>2D Motion Graphics</h2>
-        </div>
-
-        <div className={classes.infoContainer}>
-          <p className={classes.cardInfo}>
-          Whether you are a YouTuber, Twitch Streamer or simply want a professional logo animation for your business.
-          We can work together to create fun and exciting visuals for you to use online and in the videos you make
-          </p>
-        </div>
-      </div>
+      ))}
     </Section>
   )
 })
