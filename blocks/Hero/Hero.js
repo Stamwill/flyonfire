7a5e023781d3c7a76808ef1d2c0b67f34fd0be4c
William Stam <williamstam@gmail.com>
2021-06-17 12:57:11 +0100

    [Hero] add heroes

--- blocks/Hero/Hero.js
+++ blocks/Hero/Hero.js
@@ -6,28 +6,32 @@ import HeroNav from './partials'
 import classes from './Hero.module.css'
 
 const Hero = React.forwardRef(function Hero(props, ref) {
-  const { className, ...other } = props
+  const { className, heroes, heroNavs, ...other } = props
 
   return (
     <Section className={classnames(classes.root, className)} disableSpacing ref={ref} {...other}>
-      <HeroNav />
-      <div className={classes.headers}>
-        <div className={classes.mainHeading}>
-          <h1 className={classes.mainTitle}>FLYONFIRE CREATIVE</h1>
-          <h2 className={classes.subTitle}>GRAPHIC DESIGN AND ANIMATION</h2>
-        </div>
+      <HeroNav heroNavs={heroNavs}/>
+        {heroes.map((hero, id) => (
+        <div className={classes.headers} key={id}>
+          <div className={classes.mainHeading}>
+            <h1 className={classes.mainTitle}>{hero.mainTitle}</h1>
+            <h2 className={classes.subTitle}>{hero.subTitle}</h2>
+          </div>
 
-        <div className={classes.subHeading}>
-          <h2 className={classes.name}>Lee Piechowicz</h2>
-          <h3 className={classes.ideas}>Bringing your ideas to life</h3>
+          <div className={classes.subHeading}>
+            <h2 className={classes.name}>{hero.nameTitle}</h2>
+            <h3 className={classes.ideas}>{hero.ideasTitle}</h3>
+          </div>
         </div>
-      </div>
+        ))}
     </Section>
   )
 })
 
 Hero.propTypes = {
   className: PropTypes.string,
+  heroes: PropTypes.array,
+  heroNavs: PropTypes.array,
 }
 
 export default Hero
