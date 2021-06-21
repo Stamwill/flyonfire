96870db283c3cdb7009009a3f1da69e8154c6095
William <williamstam@gmail.com>
2021-06-21 16:49:58 +0100

    [Testimonials] Add block and add content

---
+++ blocks/Testimonials/Testimonials.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'clsx'
+import classes from './Testimonials.module.css'
+
+const Testimonials = React.forwardRef(function Testimonials(props, ref) {
+  const { className, references, ...other } = props
+
+  console.log(references[0].image.url)
+  return (
+    <div className={classnames(classes.root, className)} ref={ref} {...other}>
+      {references.map((ref, id) => (
+        <div className={classes.refBox} key={id}>
+          <h2 className={classes.person}>{ref.person}</h2>
+          <img className={classes.refImg} src={ref.image.url} alt="text" />
+          <p className={classes.refText}>{ref.reference.text}</p>
+        </div>
+      ))}
+    </div>
+  )
+})
+
+Testimonials.propTypes = {
+  className: PropTypes.string,
+  references: PropTypes.array
+}
+
+export default Testimonials
