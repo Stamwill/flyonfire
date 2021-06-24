623757622ee74601e6ad22d9f7b6611dd8ffd3c1
William <williamstam@gmail.com>
2021-06-24 12:57:04 +0100

    [AppDrawer] Change props, add styling

--- blocks/Hero/partials/AppDrawer.js
+++ blocks/Hero/partials/AppDrawer.js
@@ -4,13 +4,13 @@ import classes from './AppDrawer.module.css'
 import PropTypes from 'prop-types'
 
 const AppDrawer = React.forwardRef(function AppDrawer(props,ref) {
-  const { className, open, heroNavs, ...other } = props
+  const { className, open, navigations, ...other } = props
   
   return (
     <div className={classnames(classes.root, {[ classes.menuIsOpen ]: open}, className)} ref={ref} {...other}>
       <div className={classes.navBar}>
-        {heroNavs.map((nav, id) => (
-          <a key={id} className={classes.navLink} href="/">{nav.textField}</a>
+        {navigations.map((nav, id) => (
+          <a key={id} className={classes.navLink} href={nav.slug}>{nav.title}</a>
         ))}
       </div>
     </div>
