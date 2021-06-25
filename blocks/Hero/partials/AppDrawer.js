b6fb3d93e8e3c93a544072d7b94bff6bab8b48b0
William <williamstam@gmail.com>
2021-06-25 13:56:38 +0100

    [AppDrawer] Add function to remove scrolling when open

--- blocks/Hero/partials/AppDrawer.js
+++ blocks/Hero/partials/AppDrawer.js
@@ -6,6 +6,18 @@ import PropTypes from 'prop-types'
 const AppDrawer = React.forwardRef(function AppDrawer(props,ref) {
   const { className, open, navigations, ...other } = props
   
+  const toggleScroll = (state) => {
+    const body = document.body
+  
+    body.style.overflow = state
+  }
+  
+  if (open) {
+    toggleScroll('hidden')
+  } else {
+    toggleScroll()
+  }
+
   return (
     <div className={classnames(classes.root, {[ classes.menuIsOpen ]: open}, className)} ref={ref} {...other}>
       <div className={classes.navBar}>
