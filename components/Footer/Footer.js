bf4157aa1e20bfc53577ac22808f819f38d38fac
William Stam <williamstam@gmail.com>
2021-06-25 11:45:02 +0100

    [Footer] Change content

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,18 +4,26 @@ import classnames from 'clsx'
 import classes from './Footer.module.css'
 
 const Footer = React.forwardRef(function Footer(props, ref) {
-  const { className, ...other } = props
+  const { className, footers, ...other } = props
+
+  console.log(footers)
 
   return (
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
-      <h3>Hello Footer!</h3>
+      {/* {footers.map((item, id) => (
+        <div key={id}>
+          <img src={item.logo.url} alt="test" />
+          <h5>{item.title}</h5>
+        </div>
+      ))} */}
+      <h1>test</h1>
     </div>
   )
 })
 
 Footer.propTypes = {
-
   className: PropTypes.string,
+  footers: PropTypes.array
 }
 
 Footer.uiName = 'Footer'
