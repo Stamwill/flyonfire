06f2293b1deac857aa9161e766f53e47d00377b9
William <williamstam@gmail.com>
2021-06-28 12:06:32 +0100

    [HeroNav] Add Link

--- blocks/Hero/partials/HeroNav.js
+++ blocks/Hero/partials/HeroNav.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import classnames from 'clsx'
 import PropTypes from 'prop-types'
+import Link from 'next/link'
 import Image from 'next-images'
 import Hamburger from '../../../components/Hamburger'
 import classes from './HeroNav.module.css'
@@ -12,20 +13,21 @@ const HeroNav = React.forwardRef(function HeroNav(props, ref) {
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
       <div className={classes.navBar}>
         {navigations.map((nav, id) => (
-          <a
-            className={classes.navLink}
-            href={nav.slug}
-            key={id}
-          >
-            {nav.title}
-          </a>
+          <Link href={nav.slug} key={id}>
+            <a
+              className={classes.navLink}
+              href={nav.slug}
+              key={id}
+            >
+              {nav.title}
+            </a>
+          </Link>
         ))}
       </div>
       <Hamburger className={classes.hamburger} open={open} toggleMenu={toggleMenu} />
     </div>
   )
 })
-{/* <img className={classes.logo} src={navigations[0].logo.url} alt="company logo" /> */ }
 
 HeroNav.propTypes = {
   className: PropTypes.string,
