f75f7b948fb9aa549bbd58e23293afcbfa3087b8
William <williamstam@gmail.com>
2021-07-01 17:34:51 +0100

    [About] Change name on classes

--- blocks/About/About.js
+++ blocks/About/About.js
@@ -9,14 +9,16 @@ const About = React.forwardRef(function About(props, ref) {
   const { className, selfImgs, about, ...other } = props
   return (
     <Section className={classnames(classes.root, className)} ref={ref} {...other}>
-      <div className={classes.gridContainer}>
-        <div className={classes.selfImage}>
-          <img src={selfImgs[0].img.url} alt="lee" />
+      <div className={classes.aboutContainer}>
+        <div className={classes.imgContainer}>
+          <img className={classes.img} src={selfImgs[0].img.url} alt="lee" />
         </div>
 
-        <div className={classes.info}>
+        <div className={classes.textContainer}>
         {about.map((text, id) => (
-          <p key={id}>{text.textField}</p>
+          <div key={id}>
+          <p className={classes.aboutText} key={id}>{text.textField}</p>
+          </div>
         ))}
         </div>
       </div>
