7855ddce3af7607bbec7d76f6efcbcf35e64962e
William Stam <williamstam@gmail.com>
2021-07-01 11:16:39 +0100

    [Testimonials] Add Section

--- blocks/Testimonials/Testimonials.js
+++ blocks/Testimonials/Testimonials.js
@@ -1,13 +1,14 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
+import Section from '../../components/Section'
 import classes from './Testimonials.module.css'
 
 const Testimonials = React.forwardRef(function Testimonials(props, ref) {
   const { className, references, ...other } = props
 
   return (
-    <div className={classnames(classes.root, className)} ref={ref} {...other}>
+    <Section className={classnames(classes.root, className)} ref={ref} {...other}>
       {references.map((ref, id) => (
         <div className={classes.refBox} key={id}>
           <h2 className={classes.person}>{ref.person}</h2>
@@ -15,7 +16,7 @@ const Testimonials = React.forwardRef(function Testimonials(props, ref) {
           <p className={classes.refText}>{ref.reference.text}</p>
         </div>
       ))}
-    </div>
+    </Section>
   )
 })
 
