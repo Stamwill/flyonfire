c179b90d3fbbdacd5df3766408826e0a20fc74de
William Stam <williamstam@gmail.com>
2021-07-02 11:47:03 +0100

    [Gallery] Remove comment

--- blocks/Gallery/Gallery.js
+++ blocks/Gallery/Gallery.js
@@ -2,12 +2,11 @@ import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
 import classes from './Gallery.module.css'
-import { Carousel } from 'react-responsive-carousel';
-import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel'
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const Gallery = React.forwardRef(function Gallery(props, ref) {
   const { galleries, className, ...other } = props
-  console.log('ind', galleries[0].image.url)
   return (
     <div className={classnames(classes.root, className)} ref={ref} {...other}>
       <div className={classes.gallery}>
