7efed844abd8b47393e2f9f9afa8e0a613318d8c
William Stam <williamstam@gmail.com>
2021-07-12 13:15:35 +0100

    [ProjectGallery] Add Link

--- containers/ProjectGallery/ProjectGallery.js
+++ containers/ProjectGallery/ProjectGallery.js
@@ -1,8 +1,7 @@
 import * as React from 'react'
 import classnames from 'clsx'
+import Link from 'next/link'
 import classes from './ProjectGallery.module.css'
-import Gallery from '../../blocks/Gallery'
-
 
 const ProjectGallery = React.forwardRef(function ProjectGallery(props, ref) {
   const { className, projectsAnimations, projectsLogos, galleries, ...other} = props
@@ -12,7 +11,9 @@ const ProjectGallery = React.forwardRef(function ProjectGallery(props, ref) {
       {projectsLogos.map((link, id) => (
         <div className={classes.container} key={id}>
           <img className={classes.img} src={link.logo.url} alt="test"/>
-          <a className={classes.link}> {link.text} </a>
+          <Link href={link.slug} >
+            <a className={classes.link}> {link.text} </a>
+          </Link>
         </div>
       ))}
 
