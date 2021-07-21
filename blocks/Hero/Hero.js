6ad123ff277dee5a4e90894ff9e22b7134b6fc2c
William <williamstam@gmail.com>
2021-07-21 17:38:20 +0100

    [Hero] Add Image tag

--- blocks/Hero/Hero.js
+++ blocks/Hero/Hero.js
@@ -1,17 +1,32 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'clsx'
+import Image from 'next-images'
 import Section from '../../components/Section'
 import classes from './Hero.module.css'
 
 const Hero = React.forwardRef(function Hero(props, ref) {
   const { className, heroes, ...other } = props
 
+
+  // console.log(Object.values(heroes[0].heroImg))
+  console.log(heroes)
+  console.log(heroes[0])
+  console.log(heroes[0].heroImg)
+  // console.log(heroes[0].heroImg)
+  
+
   return (
     <Section className={classnames(classes.root, className)} disableSpacing ref={ref} {...other}>
-    <img className={classes.heroImg} src={heroes[0].heroImg.url} />
+    {/* <img className={classes.heroImg} src={heroes[0].heroImg.url} alt="Hero Background Image"/> */}
         {heroes.map((hero, id) => (
-        <div className={classes.headers} key={id}>
+          <div className={classes.headers} key={id}>
+          {/* <Image 
+          src={heroes[0].heroImg.url}
+          width={heroes[0].heroImg.width}
+          height={heroes[0].heroImg.height}
+          alt="Hero Background Image"
+          />   */}
           <div className={classes.mainHeading}>
             <h1 className={classes.mainTitle}>{hero.mainTitle}</h1>
             <h2 className={classes.subTitle}>{hero.subTitle}</h2>
