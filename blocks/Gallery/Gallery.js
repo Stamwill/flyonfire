afd73f3edb2fc1af7dfb80fafabef282ec5b6724
William <williamstam@gmail.com>
2021-07-27 14:20:13 +0100

    [Gallery] Remove unused props

--- blocks/Gallery/Gallery.js
+++ blocks/Gallery/Gallery.js
@@ -1,35 +1,33 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import Image from 'next/image'
-import classnames from 'clsx'
 import classes from './Gallery.module.css'
 
-const Gallery = React.forwardRef(function Gallery(props, ref) {
-  const { galleries, className, ...other } = props
+function Gallery(props) {
+  const { galleries } = props
   return (
-    <div className={classnames(classes.root, className)} ref={ref} {...other}>
+    <div className={classes.root}>
       <div className={classes.gallery}>
         {galleries.map((gallery, id) => (
           <div className={classes.galleryCard} key={id}>
             <div className={classes.galleryImg}>
             <Image 
               src={gallery.image.url}
+              rel="preload"
               layout='fill'      
               alt="logo of project"
             />
             </div>
-            {/* <img className={classes.galleryImg} src={gallery.image.url} alt="Logo Project" /> */}
             <p className={classes.galleryText}>{gallery.imageText}</p>
           </div>
         ))}
       </div>
     </div>
   )
-})
+}
 
 Gallery.propTypes = {
   galleries: PropTypes.array,
-  className: PropTypes.string,
 }
 
 Gallery.uiName = 'Gallery'
