6b0adbaf7fca6f4a8503558a501529d6ab44d489
William <williamstam@gmail.com>
2021-07-28 16:09:46 +0100

    [index.js] Add globalContext and clean up code

--- pages/index.js
+++ pages/index.js
@@ -10,90 +10,31 @@ import AppDrawer from '../blocks/Hero/partials/AppDrawer'
 import Footer from '../components/Footer'
 import AppAppBar from '../containers/AppAppBar/AppAppBar'
 import Testimonials from '../blocks/Testimonials'
-
-export default function Home({
-  heroes,
-  navigations,
-  abouts,
-  services,
-  questions,
-  testimonials,
-  selfImgs,
-  footers,
-  logos,
-}) {
-
-  const [menuIsOpen, setMenuOpen] = React.useState(false)
-
-  const toggleMenu = () => {
-    setMenuOpen((prevState) => !prevState)
-  }
-
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>FlyonFire Creative</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-        <meta name="description" content="Graphic and Animation Designer Lee Piechowicz, bringing your ideas to life" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <AppAppBar>
-        <HeroNav navigations={navigations} open={menuIsOpen} toggleMenu={toggleMenu} logo={logos} />
-        <AppDrawer navigations={navigations} open={menuIsOpen} toggleMenu={toggleMenu}/>
-      </AppAppBar>
-
-      <main>
-        <Hero heroes={heroes}/>
-        <About id="about" about={abouts} selfImgs={selfImgs} />
-        <Services id="services" services={services} />
-        <Testimonials references={testimonials} />
-        <Questions questions={questions} />
-      </main>
-
-      <footer>
-        <Footer id="footer" footers={footers} />
-      </footer>
-    </div>
-  )
-}
+import {useGlobal} from '../api/GlobalContext'
+// import client from './api/graphcms'
 
 import { GraphQLClient } from 'graphql-request'
 
 const graphcms = new GraphQLClient(process.env.GRAPHQL_URL_ENDPOINT)
 
-export async function getStaticProps() {
-  const { heroes } = await graphcms.request(
-    `
-    query Heroes() {
-      heroes {
-        mainTitle
-        subTitle
-        nameTitle
-        ideasTitle
-        image {
-          url
-          width
-          height
-        }
-      }
-    },
-    `,
-  )
-  const { navigations } = await graphcms.request(
-    `
-    query Navigations() {
-      navigations {
-        title
-        slug
+export async function getServerSideProps() {
+  const { heroes } = await graphcms.request(`
+  query {
+    heroes {
+      mainTitle
+      subTitle
+      nameTitle
+      ideasTitle
+      image {
+        url
+        width
+        height
       }
     }
-    `,
-  )
+  },
+`
+)
+
   const { abouts } = await graphcms.request(
     `
     query Abouts() {
@@ -163,41 +104,56 @@ export async function getStaticProps() {
     `
   )
 
-  const { footers } = await graphcms.request(
-    `
-    query footers() {
-      footers {
-        title
-      }
-    }
-    `
-  )
-
-  const { logos } = await graphcms.request(
-    `
-    query logos() {
-      logos {
-        logo {
-          url
-          width
-          height
-        }
-      }
-    }
-    `
-  )
-
   return {
     props: {
       heroes,
-      navigations,
       abouts,
       services,
       questions,
       testimonials,
       selfImgs,
-      footers,
-      logos,
     },
   }
 }
+
+export default function Home({
+  heroes,
+  abouts,
+  services,
+  questions,
+  testimonials,
+  selfImgs,
+}) {
+
+  const { menu, settings: {footer, logos} } = useGlobal()
+
+  const [menuIsOpen, setMenuOpen] = React.useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState)
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta name="description" content="Graphic and Animation Designer Lee Piechowicz, bringing your ideas to life" />
+      </Head>
+
+      <AppAppBar>
+        <HeroNav navigations={menu} open={menuIsOpen} toggleMenu={toggleMenu} logo={logos} />
+        <AppDrawer navigations={menu} open={menuIsOpen} toggleMenu={toggleMenu}/>
+      </AppAppBar>
+
+      <main>
+        <Hero heroes={heroes} />
+        <About id="about" about={abouts} selfImgs={selfImgs} />
+        <Services id="services" services={services} />
+        <Testimonials references={testimonials} />
+        <Questions questions={questions} />
+      </main>
+     
+      <Footer id="footer" footers={footer} />
+    </div>
+  )
+}
+
