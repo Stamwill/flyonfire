08dc58f9c7eb5fce5e44c960d11595b132fa704d
William Stam <williamstam@gmail.com>
2021-07-29 12:38:40 +0100

    [_app.js] Fix requests into one request

--- pages/_app.js
+++ pages/_app.js
@@ -14,56 +14,45 @@ function MyApp({ cmsProps, Component, pageProps }) {
 
 MyApp.getInitialProps = async (props) => {
   const { Component, ctx } = props
-  
+
   let cmsProps = {}
   if (ctx.req) {
-      const { navigations } = await graphcms.request(
-        `
-          query navigations() {
-          navigations {
-            title
-            slug
-          }
-        }`
-      )
-
-      const { footers } = await graphcms.request(
-        `
-          query footers() {
-            footers {
-              title
-            }
-          }`
-      )
-      const { logos } = await graphcms.request(
-        `
-        query logos() {
-          logos {
-            logo {
-              url
-            }
-          }
+    const { navigations, footers, logos } = await graphcms.request(
+      `
+        query {
+        navigations {
+          title
+          slug
         }
-        `
-      )
-      cmsProps = {
-        menu: navigations,
-        settings: {
-          footer: footers,
-          logos,
+        footers {
+          title
+        }
+        logos {
+          logo {
+            url
+          }
         }
       }
+      `
+    )
+    cmsProps = {
+      menu: navigations,
+      settings: {
+        footer: footers,
+        logos,
+      }
     }
+  }
 
-    let pageProps = {}
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
+  let pageProps = {}
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
 
-    return {
-      cmsProps,
-      pageProps,
-    }
+  return {
+    cmsProps,
+    pageProps,
+  }
 }
 
 export default MyApp
