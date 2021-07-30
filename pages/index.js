98cb7d3fc11402efbd8ff4cea6b710ab8abda9a3
William <williamstam@gmail.com>
2021-07-30 14:58:16 +0100

    [index.js] Add meta and title to Head

--- pages/index.js
+++ pages/index.js
@@ -102,7 +102,14 @@ export default function Home({
   return (
     <div className={styles.container}>
       <Head>
+        <title>FlyonFire Creative</title>
         <meta name="description" content="Graphic and Animation Designer Lee Piechowicz, bringing your ideas to life" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <AppAppBar>
