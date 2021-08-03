94730ce6cc107d58d9ca466f88254e6b53da4bab
William <williamstam@gmail.com>
2021-08-03 16:09:08 +0100

    [About] Add margin to text

--- blocks/About/About.js
+++ blocks/About/About.js
@@ -22,11 +22,11 @@ const About = React.forwardRef(function About(props, ref) {
         </div>
 
         <div className={classes.textContainer}>
-        {about.map((text, id) => (
-          <div key={id}>
-          <p className={classes.aboutText} key={id}>{text.textField}</p>
-          </div>
-        ))}
+          {about.map((text, id) => (
+            <div key={id}>
+              <p className={classes.aboutText} key={id}>{text.textField}</p>
+            </div>
+          ))}
         </div>
       </div>
     </Section>
